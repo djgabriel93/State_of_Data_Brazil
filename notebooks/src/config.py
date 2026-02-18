788eabadc6117c7de966919b44374fae4a8b9d37
Gabriel Duarte <65737170+djgabriel93@users.noreply.github.com>
2026-02-18 12:40:54 -0300

    analise dos dados

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -7,6 +7,7 @@ PASTA_DADOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
 DADOS_ORIGINAIS = PASTA_DADOS / "State_of_Data_2024.csv"
+DADOS_TRATADO = PASTA_DADOS / "State_of_Data_2024_tratado.parquet"
 
 # coloque abaixo o caminho para os arquivos de modelos de seu projeto
 PASTA_MODELOS = PASTA_PROJETO / "modelos"
